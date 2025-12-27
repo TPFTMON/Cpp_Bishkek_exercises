ed6c42a2e892885b1a057accc377a8929402c3ff
TPFTMON <tpftmoon@gmail.com>
2025-12-27 02:02:04 +0100

    lesson_10: L is done; J doesn't want to cooperate.

--- lesson_10/J_ex/my_J.cpp
+++ lesson_10/J_ex/my_J.cpp
@@ -1,64 +1,100 @@
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 
-int roman_to_arab(std::string r) {
-    int val = 0;
-    for (int i = 0; i < r.length(); i++) {
-        int v1 = 0;
-        if (r[i] == 'I') v1 = 1;
-        else if (r[i] == 'V') v1 = 5;
-        else if (r[i] == 'X') v1 = 10;
-        else if (r[i] == 'L') v1 = 50;
-        else if (r[i] == 'C') v1 = 100;
-        else if (r[i] == 'D') v1 = 500;
-        else if (r[i] == 'M') v1 = 1000;
-        int v2 = 0;
-        if (i + 1 < r.length()) {
-            if (r[i+1] == 'I') v2 = 1;
-            else if (r[i+1] == 'V') v2 = 5;
-            else if (r[i+1] == 'X') v2 = 10;
-            else if (r[i+1] == 'L') v2 = 50;
-            else if (r[i+1] == 'C') v2 = 100;
-            else if (r[i+1] == 'D') v2 = 500;
-            else if (r[i+1] == 'M') v2 = 1000;
-        }
-        if (v1 < v2) {
-            val += v2 - v1;
-            i++;
-        } else {
-            val += v1;
-        }
-    }
-    return val;
+int	roman_to_arab(std::string r)
+{
+	int	val;
+	int	v1;
+	int	v2;
+
+	val = 0;
+	for (int i = 0; i < r.length(); i++)
+	{
+		v1 = 0;
+		if (r[i] == 'I')
+			v1 = 1;
+		else if (r[i] == 'V')
+			v1 = 5;
+		else if (r[i] == 'X')
+			v1 = 10;
+		else if (r[i] == 'L')
+			v1 = 50;
+		else if (r[i] == 'C')
+			v1 = 100;
+		else if (r[i] == 'D')
+			v1 = 500;
+		else if (r[i] == 'M')
+			v1 = 1000;
+		v2 = 0;
+		if (i + 1 < r.length())
+		{
+			if (r[i + 1] == 'I')
+				v2 = 1;
+			else if (r[i + 1] == 'V')
+				v2 = 5;
+			else if (r[i + 1] == 'X')
+				v2 = 10;
+			else if (r[i + 1] == 'L')
+				v2 = 50;
+			else if (r[i + 1] == 'C')
+				v2 = 100;
+			else if (r[i + 1] == 'D')
+				v2 = 500;
+			else if (r[i + 1] == 'M')
+				v2 = 1000;
+		}
+		if (v1 < v2)
+		{
+			val += v2 - v1;
+			i++;
+		}
+		else
+		{
+			val += v1;
+		}
+	}
+	return (val);
 }
 
-int main() {
-    std::string s;
-    std::getline(std::cin, s);
-    std::string result = "";
-    std::string roman = "";
-    for (int i = 0; i < s.length(); i++){
-        char c = s[i];
-        if (c == 'I' || c == 'V' || c == 'X'|| c == 'L'|| c == 'C' || c == 'D'|| c == 'M'){
-            roman += c;
-        } else {
-            if (!roman.empty()) {
-                int num = roman_to_arab(roman);
-                std::ostringstream oss;
-                oss << num;
-                result += oss.str();
-                roman = "";
-            }
-            result += c;
-        }
-    }
-    if (!roman.empty()) {
-        int num = roman_to_arab(roman);
-        std::ostringstream oss;
-        oss << num;
-        result += oss.str();
-    }
-    std::cout << result << std::endl;
-    return 0;
+int	main(void)
+{
+	char	c;
+	int		num;
+	int		num;
+
+	std::string s;
+	std::getline(std::cin, s);
+	std::string result = "";
+	std::string roman = "";
+	for (int i = 0; i < s.length(); i++)
+	{
+		c = s[i];
+		if (c == 'I' || c == 'V' || c == 'X' || c == 'L' || c == 'C' || c == 'D'
+			|| c == 'M')
+		{
+			roman += c;
+		}
+		else
+		{
+			if (!roman.empty())
+			{
+				num = roman_to_arab(roman);
+				std::ostringstream oss;
+				oss << num;
+				result += oss.str();
+				roman = "";
+			}
+			result += c;
+		}
+	}
+	if (!roman.empty())
+	{
+		num = roman_to_arab(roman);
+		std::ostringstream oss;
+		oss << num;
+		result += oss.str();
+	}
+	std::cout << result << std::endl;
+	return (0);
 }
--- lesson_10/L_ex/my_L.cpp
+++ lesson_10/L_ex/my_L.cpp
@@ -1,46 +1,71 @@
-// #include <iostream>
-// #include <string>
+#include <iostream>
+#include <string>
 
-// struct Name {
-//     std::string full;
-//     std::string surname;
-//     std::string init;
-// };
+struct Name {
+    std::string full;
+    std::string last_name;
+    std::string init;
+};
 
-// void quickSort(Name* names, int l, int r) {
-//     if (l >= r) return;
-//     Name pivot = names[(l + r) / 2];
-//     int i = l, j = r;
-//     while (i <= j) {
-//         int cmp1 = names[i].surname.compare(pivot.surname);
-//         int cmp2 = 0;
-//         if (cmp1 == 0) cmp2 = names[i].init.compare(pivot.init);
-//         while (cmp1 < 0 || (cmp1 == 0 && cmp2 < 0)) {
-//             i++;
-//             cmp1 = names[i].surname.compare(pivot.surname);
-//             cmp2 = 0;
-//             if (cmp1 == 0) cmp2 = names[i].init.compare(pivot.init);
-//         }
-//         cmp1 = names[j].surname.compare(pivot.surname);
-//         cmp2 = 0;
-//         if (cmp1 == 0) cmp2 = names[j].init.compare(pivot.init);
-//         while (cmp1 > 0 || (cmp1 == 0 && cmp2 > 0)) {
-//             j--;
-//             cmp1 = names[j].surname.compare(pivot.surname);
-//             cmp2 = 0;
-//             if (cmp1 == 0) cmp2 = names[j].init.compare(pivot.init);
-//         }
-//         if (i <= j) {
-//             Name temp = names[i];
-//             names[i] = names[j];
-//             names[j] = temp;
-//             i++;
-//             j--;
-//         }
-//     }
-//     quickSort(names, l, j);
-//     quickSort(names, i, r);
-// }
+void quick_sort(Name* names, int l, int r) {
+    if (l >= r) return;
+    Name pivot = names[(l + r) / 2];
+    int i = l, j = r;
+    while (i <= j) {
+        int cmp1 = names[i].last_name.compare(pivot.last_name);
+        int cmp2 = 0;
+        if (cmp1 == 0) cmp2 = names[i].init.compare(pivot.init);
+        while (cmp1 < 0 || (cmp1 == 0 && cmp2 < 0)) {
+            i++;
+            cmp1 = names[i].last_name.compare(pivot.last_name);
+            cmp2 = 0;
+            if (cmp1 == 0) cmp2 = names[i].init.compare(pivot.init);
+        }
+        cmp1 = names[j].last_name.compare(pivot.last_name);
+        cmp2 = 0;
+        if (cmp1 == 0) cmp2 = names[j].init.compare(pivot.init);
+        while (cmp1 > 0 || (cmp1 == 0 && cmp2 > 0)) {
+            j--;
+            cmp1 = names[j].last_name.compare(pivot.last_name);
+            cmp2 = 0;
+            if (cmp1 == 0) cmp2 = names[j].init.compare(pivot.init);
+        }
+        if (i <= j) {
+            Name temp = names[i];
+            names[i] = names[j];
+            names[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    quick_sort(names, l, j);
+    quick_sort(names, i, r);
+}
+
+int main() {
+    int k;
+    std::cin >> k;
+    std::cin.ignore();
+    Name names[20];
+    for (int i = 0; i < k; i++) {
+        std::string line;
+        std::getline(std::cin, line);
+        // size_t dot = line.find('.');
+        // if (dot != std::string::npos) {
+        names[i].full = line.substr();
+        size_t space = names[i].full.rfind(' ');
+        if (space != std::string::npos) {
+            names[i].init = names[i].full.substr(0, space);
+            names[i].last_name = names[i].full.substr(space + 1);
+        }
+        // }
+    }
+    quick_sort(names, 0, k-1);
+    for (int i = 0; i < k; i++) {
+        std::cout << names[i].init << " " << names[i].last_name << std::endl;
+    }
+    return 0;
+}
 
 // int main() {
 //     int k;
@@ -52,100 +77,17 @@
 //         std::getline(std::cin, line);
 //         size_t dot = line.find('.');
 //         if (dot != std::string::npos) {
-//             names[i].full = line.substr(dot + 1);
+//             names[i].full = line.substr(dot + 2);
 //             size_t space = names[i].full.rfind(' ');
 //             if (space != std::string::npos) {
 //                 names[i].init = names[i].full.substr(0, space);
-//                 names[i].surname = names[i].full.substr(space + 1);
+//                 names[i].last_name = names[i].full.substr(space + 1);
 //             }
 //         }
 //     }
-//     quickSort(names, 0, k-1);
+//     quick_sort(names, 0, k-1);
 //     for (int i = 0; i < k; i++) {
-//         std::cout << names[i].init << " " << names[i].surname << std::endl;
+//         std::cout << names[i].init << " " << names[i].last_name << std::endl;
 //     }
 //     return 0;
 // }
-
-// #include <algorithm>
-
-// void quick_sort(std::string w[], int l, int r) {
-//     if (l >= r)
-//         return;
-//     std::string pivot = w[(l + r) / 2];
-//     int i = l, j = r;
-//     while (i <= j) {
-//         while (w[i] < pivot){
-//             i++;
-//         }
-//         while (w[j] > pivot){
-//             j--;
-//         }
-//         if (i <= j) {
-//             std::string temp = w[i];
-//             w[i] = w[j];
-//             w[j] = temp;
-//             i++;
-//             j--;
-//         }
-//     }
-//     quick_sort(w, l, j);
-//     quick_sort(w, i, r);
-// }
-
-// struct Person {
-//     std::string initials;
-//     std::string surname;
-//     std::string full;
-
-//     // Operator overloading for sorting
-//     bool operator<(const Person& other) const {
-//         if (surname != other.surname) {
-//             return surname < other.surname;
-//         }
-//         return initials < other.initials;
-//     }
-// };
-
-// int main()
-// {
-//     const int MAX_K = 25;
-//     Person people[MAX_K];
-//     int k;
-//     if (std::cin >> k) {
-//         std::string dummy;
-//         std::getline(std::cin, dummy);
-
-//         for (int i = 0; i < k; ++i) {
-//             std::string line;
-//             std::getline(std::cin, line);
-//             // Format: "1. A.A. Surname"
-//             size_t dotPos = line.find('.');
-//             size_t start = dotPos + 1;
-//             while (start < line.length() && line[start] == ' ') start++;
-
-//             std::string content = line.substr(start);
-//             // Content is "A.A. Surname"
-//             // Find the last space to split Initials and Surname
-//             size_t lastSpace = content.rfind(' ');
-
-//             people[i].full = content;
-//             if (lastSpace != std::string::npos) {
-//                 people[i].initials = content.substr(0, lastSpace);
-//                 people[i].surname = content.substr(lastSpace + 1);
-//             } else {
-//                 people[i].surname = content; // Fallback
-//             }
-//         }
-
-//         // quick_sort();
-//         std::sort(people, people + k);
-
-//         for (int i = 0; i < k; ++i) {
-//             std::cout << people[i].full << std::endl;
-//         }
-//     }
-
-//     return (0);
-// }
-
